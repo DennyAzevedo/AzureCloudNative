9ae830e188baedfdb9a7e2883f753941ed53d8d9
Denny Azevedo <denny.filho@animaeducacao.com.br>
2025-06-13 11:52:30 -0300

    Upload da imagem

--- Lab01/main.py
+++ Lab01/main.py
@@ -17,14 +17,28 @@ SQLUser = os.getenv("SQL_USER")
 SQLPassword = os.getenv("SQL_PASSWORD")
 
 st.title("Cadastro de Produtos")
-
+# Formulário de Cadastro de Produto
 product_name = st.text_input("Nome do Produto")
 product_description = st.text_area("Descrição do Produto")
 product_price = st.number_input("Preço do Produto", min_value=0.0, format="%.2f")
 product_image = st.file_uploader("Imagem do Produto", type=["jpg", "jpeg", "png"])
 
+#Save image on blob storage
+def upload_blob(file):
+	blob_service_client = BlobServiceClient.from_connection_string(BlobConnectionString)
+	container_client = blob_service_client.get_container_client(BlobContainerName)
+	blob_name = str(uuid.uuid4()) + file.name  # Generate a unique blob name
+	blob_client = container_client.get_blob_client(blob_name)
+	blob_client.upload_blob(file.read(), overwrite=True)
+	image_url = f"https://{BlobAccountName}.blob.core.windows.net/{BlobContainerName}/{blob_name}"
+	return image_url
+
 if st.button("Cadastrar Produto"):
-	return_message = "Produto cadastrado com sucesso!"
+	if product_image is not None:
+		image_url = upload_blob(product_image)
+		return_message = f"Produto cadastrado com sucesso! Imagem disponível em: {image_url}"
+	else:
+		return_message = "Produto cadastrado com sucesso! (sem imagem)"
 
 st.header("Produtos Cadastrados")
 
